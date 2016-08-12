0b0e07f64bf2d91db63fb3c598e7aa141ef6fde2
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2016-08-12 10:59:05 +0200

    refactoring use await accept enter to start game on everything, even without focus on start button

--- web/ld35.dart
+++ web/ld35.dart
@@ -1,48 +1,52 @@
 import 'package:ld35/client.dart';
 
-void main() {
-  new Game().start().then((game) {
-    game.stop();
-    querySelector('#loading').style.display = 'none';
-    (querySelector('#startGame') as ButtonElement)
-      ..style.display = 'inline-block'
-      ..focus()
-      ..onClick.listen((event) {
-        startGame();
-      });
+Game game;
+
+Future<Null> main() async {
+  game = await new Game().start();
+  game.stop();
+  querySelector('#loading').style.display = 'none';
+  (querySelector('#startGame') as ButtonElement)
+    ..style.display = 'inline-block';
+  querySelector('#startGame').onClick.listen((_) {
+    if (game.isStopped) {
+      startGame();
+    }
+  });
+  querySelector('body').onKeyDown.listen((event) {
+    if (game.isStopped && event.keyCode == KeyCode.ENTER) {
+      startGame();
+    }
   });
 }
 
-void startGame() {
-  new Game().start().then((Game game) {
-    game.pause();
-    game.startSpeed = double.parse(
-        (querySelector('input[type=radio][name=speed]:checked')
-                as RadioButtonInputElement)
-            .value);
-    querySelector('#storyContainer').style..opacity = '0.0';
-    querySelector('body').style.cursor = 'none';
-    querySelector('#game').style.opacity = '1.0';
-    querySelector('#hud').style.opacity = '1.0';
+Future<Null> startGame() async {
+  game = await new Game().start();
+  game.pause();
+  game.startSpeed = double.parse(
+      (querySelector('input[type=radio][name=speed]:checked')
+              as RadioButtonInputElement)
+          .value);
+  querySelector('#storyContainer').style..opacity = '0.0';
+  querySelector('body').style.cursor = 'none';
+  querySelector('#game').style.opacity = '1.0';
+  querySelector('#hud').style.opacity = '1.0';
 
-    new Timer(new Duration(seconds: 1), () {
-      game.resume();
-      querySelector('#storyContainer').style.display = 'none';
-      game.onGameOver().then((score) {
-        game.stop();
-        querySelector('#lastscore').text = '$score';
-        if (int.parse(querySelector('#highscore').text) < score) {
-          querySelector('#highscore').text = '$score';
-        }
-        querySelector('#storyContainer').style
-          ..opacity = '1.0'
-          ..display = 'flex'
-          ..cursor = 'inherit';
-        querySelector('#game').style.opacity = '0.1';
-        querySelector('#hud').style.opacity = '0.1';
-        querySelector('body').style.cursor = 'inherit';
-        querySelector('#startGame').focus();
-      });
-    });
+  await new Future.delayed(new Duration(seconds: 1));
+  game.resume();
+  querySelector('#storyContainer').style.display = 'none';
+  game.onGameOver().then((score) {
+    game.stop();
+    querySelector('#lastscore').text = '$score';
+    if (int.parse(querySelector('#highscore').text) < score) {
+      querySelector('#highscore').text = '$score';
+    }
+    querySelector('#storyContainer').style
+      ..opacity = '1.0'
+      ..display = 'flex'
+      ..cursor = 'inherit';
+    querySelector('#game').style.opacity = '0.1';
+    querySelector('#hud').style.opacity = '0.1';
+    querySelector('body').style.cursor = 'inherit';
   });
 }
